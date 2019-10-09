2b766006d488014a6ba5a208ff2d93a36fad2c8d
Sourav Goswami <souravgoswami@protonmail.com>
2019-10-10 03:48:50 +0530

    Add files via upload

---
+++ vcf_reader(ruby 2.3).rb
@@ -0,0 +1,60 @@
+Hash.class_exec { define_method(:fetch_r) { |reg| keys.select { |x| x[reg] }.map! { |x| self[x] } } }
+
+Kernel.class_exec { define_method(:then) { |&block| block === self } } unless defined?(Kernel.then)
+
+PATH = File.dirname($0)
+
+VCF = Class.new do
+	# Append data to a vcf file / create a new vcf file
+	define_singleton_method(:create) do |name, n:, fn:, telcell: nil, telwork: nil, telhome: nil, telhomefax: nil|
+		telcell = telcell ? "\r\nTEL;CELL:#{telcell.to_i}" : ''
+		telwork = telwork ? "\r\nTEL;WORK:#{telwork.to_i}" : ''
+		telhome = telhome ? "\r\nTEL;HOME:#{telhome.to_i}" : ''
+		telhomefax = telhomefax ? "\r\nTEL;HOME;FAX:#{telhomefax.to_i}" : ''
+
+		data = <<~EOF.tap { |x|  }
+			BEGIN:VCARD\r
+			VERSION:2.1\r
+			N:;#{n};;;\r
+			FN:#{fn}#{telcell}#{telwork}#{telhome}#{telhomefax}\r
+			END:VCARD\r
+		EOF
+
+		begin
+			n = name.end_with?('vcf') ? name : "#{name}.vcf"
+			if IO.read(n).include?(data)
+				warn("#{n} already contains the data:\n#{data.lines.map { |x| "\t#{x}" }.join}The file #{File.basename(n)} is not written...")
+			else
+				File.open(n, ?a) { |x| x.puts(data) }
+			end
+		rescue Errno::EACCES
+			STDERR.puts "Permission denied while attempted to write to #{n}"
+		rescue Exception
+			STDERR.puts $!
+			STDERR.puts "Uh oh! An Error Just Occurred!", $!.backtrace.map! { |x| "\t#{x}" }
+		end
+
+		data
+	end
+
+	def initialize(data)
+		@data = data.split('END:VCARD').map! { |x| x.tap(&:strip!).split(/\r\n/) }.reject!(&:empty?).map!(&:freeze).freeze
+		@arr = @data.map { |x| x.drop(2).reduce({}) { |z, y| z.merge!(y.split(?:).then { |z| {z[0].to_s.gsub(?;, ?-) => z[1].to_s.gsub(?;, ?\s).tap(&:strip!) } }) } }.freeze
+	end
+
+	def search(field)
+		f = field.downcase
+		@arr.select { |x| x.values.any? { |y| y.downcase.then { |z| z.include?(f) || z.delete(?\s).include?(f) } } }
+	end
+
+	define_method(:keys) { @arr.map(&:keys) }
+	define_method(:values) { @arr.map(&:values) }
+	define_method(:to_s) { "<<Class: #{send(:class)}> <Variables: #{instance_variables.join(', ')}> <Methods: #{public_methods.reverse.drop(56).join(', ')}>>" }
+	def [](arg) @arr.map { |x| x.fetch_r(arg)[0] }.tap(&:compact!) end
+
+	define_method(:fetch) { |key1, key2| @arr.map { |x| {key1 => x.fetch_r(key2)} if x.values.any? { |y| y[key1] || y[key2] } }.tap(&:compact!) }
+	define_method(:to_h) { @arr }
+
+	alias :inspect :to_s
+	alias :to_hash :to_h
+end
